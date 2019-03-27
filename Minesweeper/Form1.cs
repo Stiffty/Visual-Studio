55c627caf3c3674c591d53e76ae657320955214d
elach <elach@DESKTOP-0BLGD50>
2019-03-27 22:00:59 +0100

    Minesweeper(Noch Bugs)

--- Minesweeper/Form1.cs
+++ Minesweeper/Form1.cs
@@ -12,54 +12,178 @@ namespace Minesweeper
 {
     public partial class Form1 : Form
     {
+        public int B_Size = 25;
+        public static int grigsize = 30;
+        public  int[,] Feld = new int[grigsize, grigsize];
+        
+
         public Form1()
         {
             InitializeComponent();
+            
+            for (int i = 0; i < grigsize; i++)
+            {
+                for (int j = 0; j < grigsize; j++)
+                {
+                    Button b = new Button();
+                    b.Click += ButtenClick;
+                    b.MouseMove += ButtenMove;
+                    b.MouseDown += ButtenDClick;
+                    b.Size = new Size(B_Size, B_Size);
+                    b.Location = new Point((i * B_Size), (j * B_Size));
+                    b.Show();
+                    this.Controls.Add(b);
+                    
+                }
+            }
+            Random rand = new Random();
+            for (int i = 0; i < 30; i++)
+            {
+               
+                    Feld[rand.Next(grigsize - 1), rand.Next(grigsize - 1)] = -1;
+                
+            }
+            
+             
+            for (int i = 0; i < grigsize; i++)
+            {
+                for (int j = 0; j < grigsize; j++)
+                {
+                    if (Feld[i,j] != -1) {
+                        Feld[i,j] = 0;
+                        
+                            try
+                            {
+                            if (Feld[i, j - 1] == -1)
+                            {
+                                Feld[i, j]++;
+                            }
+                        }
+                            catch 
+                            {
+
+                               
+                            }
+                        try
+                        {
+                            if (Feld[i, j + 1] == -1)
+                            {
+                                Feld[i, j]++;
+                            }
+                        }
+                        catch
+                        {
+
+
+                        }
+                        try
+                        {
+                            if (Feld[i-1, j] == -1)
+                            {
+                                Feld[i, j]++;
+                            }
+                        }
+                        catch
+                        {
+
+
+                        }
+                        try
+                        {
+                            if (Feld[i + 1, j] == -1)
+                            {
+                                Feld[i, j]++;
+                            }
+                        }
+                        catch
+                        {
+
+
+                        }
+                        try
+                        {
+                            if (Feld[i + 1, j+1] == -1)
+                            {
+                                Feld[i, j]++;
+                            }
+                        }
+                        catch
+                        {
+
+
+                        }
+                        try
+                        {
+                            if (Feld[i - 1, j - 1] == -1)
+                            {
+                                Feld[i, j]++;
+                            }
+                        }
+                        catch
+                        {
+
+
+                        }
+                        try
+                        {
+                            if (Feld[i + 1, j - 1] == -1)
+                            {
+                                Feld[i, j]++;
+                            }
+                        }
+                        catch
+                        {
+
+
+                        }
+                        try
+                        {
+                            if (Feld[i - 1, j + 1] == -1)
+                            {
+                                Feld[i, j]++;
+                            }
+                        }
+                        catch
+                        {
+
+
+                        }
+
+
+                    }
+
+                }
+            
+            }
+        }
+        
+        private void ButtenDClick(object sender, MouseEventArgs e)
+        {
+            Button b = sender as Button;
+            int x = b.Location.X / B_Size;
+            int y = b.Location.Y / B_Size;
+            b.Text = "B";
+            b.BackColor = Color.Blue;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void ButtenClick(object sender, EventArgs e)
         {
-
+            Button b = sender as Button;
+            int x=b.Location.X / B_Size;
+            int y = b.Location.Y / B_Size;
+            b.Text = (Feld[x,y]).ToString();
+            b.BackColor = Color.Red;
         }
-
-        private void button2_Click(object sender, EventArgs e)
+       private void ButtenMove(object sender, EventArgs e)
         {
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-
+            Button b = sender as Button;
+            int x = b.Location.X / B_Size;
+            int y = b.Location.Y / B_Size;
+            if (Feld[x, y] == 0)
+            {
+                b.Text = (Feld[x, y]).ToString();
+                b.BackColor = Color.Red;
+            }
         }
     }
 }
