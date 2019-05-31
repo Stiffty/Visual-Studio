19baa4065bec8164eea28e49f7b9e60d78324558
elach <elach@DESKTOP-0BLGD50>
2019-05-31 21:14:21 +0200

    Snake

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(300, 200);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(25, 25);
+            this.panel1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 761);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Snake/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        private int Richtung = 0;
+        private int lvl = 300;
+        private int x = 300;
+        private int y = 200;
+
+        Panel[,] Panel = new Panel[800,800];
+
+        private Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            ziel();
+            timer1.Interval = lvl;
+        }
+
+        private void Form1_KeyDown(object sender,KeyEventArgs e)
+        {
+
+
+            if ( e.KeyCode == Keys.S )
+            {
+                Richtung = 2;
+            }
+            else
+
+            if ( e.KeyCode == Keys.W )
+            {
+                Richtung = 0;
+            }
+            else
+            if ( e.KeyCode == Keys.A )
+            {
+                Richtung = 3;
+            }
+            else
+            if ( e.KeyCode == Keys.D )
+            {
+                Richtung = 1;
+            }
+        }
+
+        private void timer1_Tick(object sender,EventArgs e)
+        {
+           
+
+                if (!Zieltest())
+                {
+                    if (Richtung == 1)
+                    {
+                        x += 25;
+                    }
+                    else if (Richtung == 2)
+                    {
+                        y += 25;
+                    }
+                    else if (Richtung == 3)
+                    {
+                        x -= 25;
+                    }
+                    else if (Richtung == 0)
+                    {
+                        y -= 25;
+                    }
+
+                    panel1.Location = new System.Drawing.Point(x, y);
+
+                }
+                else
+                {
+                    Panel = new Panel[800,800];
+                    lvl -= 10;
+                    timer1.Interval = lvl;
+                    ziel();
+                }
+
+            
+        }
+
+        public void ziel()
+        {
+            int x = rand.Next(0,775);
+            int y = rand.Next(0,775);
+
+            Panel P = new Panel
+            {
+                Size = new Size(25,25),
+                Location = new Point(( x ),( y )),
+                BackColor = Color.White
+            };
+            P.Show();
+            Controls.Add(P);
+
+            
+                for ( int i = x ; i < ( x + 25 ) ; i++ )
+                {
+                    for ( int j = y ; j < ( y + 25 ) ; j++ )
+                    {
+                        Panel[i,j] = P;
+                    }
+                }
+            
+        }
+
+       
+
+        private Boolean Zieltest()
+        {
+            try
+            {
+                for ( int i = this.x ; i < ( x + 25 ) ; i++ )
+                {
+                    for ( int j = this.y ; j < ( y + 25 ) ; j++ )
+                    {
+                        if ( Panel[i,j] != null )
+                        {
+                            Controls.Remove(Panel[i,j]);
+                            return true;
+                        }
+                    }
+                }
+
+                return false;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            
+        }
+    }
+
+}
